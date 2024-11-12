af12b7130f616e36e577198a1fe397e52c13062f
pablo <pablo@hypothetic.art>
2024-11-12 13:34:26 +0900

    testing with new reflex version and with explicit port setting in the config file

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="reflex_test_deploy",
+    frontend_port=10000,
+    backend_port=10000,
     api_url="https://myfeedtldr.onrender.com",
+    backend_host="0.0.0.0",
     # api_url="https://reflextestdeploy-production.up.railway.app",
 )
