93432b260794ec389ae49986dc7040ee121dc693
pablo <pablo@hypothetic.art>
2024-11-12 12:24:01 +0900

    changed api_uel back to render.com

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="your_app_name",
-    api_url="https://reflextestdeploy-production.up.railway.app",
+    api_url="https://myfeedtldr.onrender.com",
+    # api_url="https://reflextestdeploy-production.up.railway.app",
 )
