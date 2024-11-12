2fb756e73225e099f693a27fa8b40138832bc619
pablo <pablo@hypothetic.art>
2024-11-12 12:41:33 +0900

    fixed bug in rxconfig file

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="your_app_name",
+    app_name="reflex_test_deploy",
     api_url="https://myfeedtldr.onrender.com",
     # api_url="https://reflextestdeploy-production.up.railway.app",
 )
