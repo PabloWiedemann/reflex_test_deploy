51c8d1b37e4ef258c4fa02694ff67056cbc52f76
pablo <pablo@hypothetic.art>
2024-11-16 16:45:35 +0900

    added dockerfile and caddy file from the reflex example with one single port

--- rxconfig.py
+++ rxconfig.py
@@ -2,10 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="reflex_test_deploy",
+    backend_host="0.0.0.0",
     # frontend_port=10000,
     # backend_port=10000,
-    # api_url="https://myfeedtldr.onrender.com",
-    # api_url="https://reflex-test.up.railway.app",
-    backend_host="0.0.0.0",
-    api_url="https://reflex-test-deploy.replit.app",
+    api_url="https://reflex-test-deploy.onrender.com", # Render
+    # api_url="https://reflex-test.up.railway.app", # Railway
+    # api_url="https://reflex-test-deploy.replit.app", # Replit
 )
