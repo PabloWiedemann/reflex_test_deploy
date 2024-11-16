71a60f3c3a833bd08294adf3003680b284880882
pablo <pablo@hypothetic.art>
2024-11-16 15:19:02 +0900

    change api_url to replit

--- rxconfig.py
+++ rxconfig.py
@@ -5,6 +5,7 @@ config = rx.Config(
     # frontend_port=10000,
     # backend_port=10000,
     # api_url="https://myfeedtldr.onrender.com",
+    # api_url="https://reflex-test.up.railway.app",
     backend_host="0.0.0.0",
-    api_url="https://reflex-test.up.railway.app",
+    api_url="https://reflex-test-deploy.replit.app",
 )
