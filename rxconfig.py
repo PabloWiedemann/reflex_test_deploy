d7f280f3effdb64531592b094d7fe1f73eeaef7e
pablo <pablo@hypothetic.art>
2024-11-16 14:42:26 +0900

    change api_url back to railway

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="reflex_test_deploy",
-    frontend_port=10000,
-    backend_port=10000,
-    api_url="https://myfeedtldr.onrender.com",
+    # frontend_port=10000,
+    # backend_port=10000,
+    # api_url="https://myfeedtldr.onrender.com",
     backend_host="0.0.0.0",
-    # api_url="https://reflex-test.up.railway.app",
+    api_url="https://reflex-test.up.railway.app",
 )
